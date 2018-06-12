f92b04be63b8189bfd0e9e33ba4dca7b994f9c73
Alena <asheras@mail.ru>
2018-06-12 17:19:53 +0300

    Created Database with all necessary properties!

---
+++ Core/Model/Control.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class Control
+    {
+        public int Id { get; set; }
+        public bool Sweep { get; set; }
+        public bool Wash { get; set; }
+        public bool Trash { get; set; }
+        public DateTime When { get; set; }
+        public virtual User Executer { get; set; }
+        public virtual User Checker { get; set; }
+    }
+}
