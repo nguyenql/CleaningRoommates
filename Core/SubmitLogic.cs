8b6e683f7e3850b0cd498f41177476e128e7bbf1
Цой Галина <gtsoy@edu.hse.ru>
2018-06-13 14:23:10 +0300

    add cg in submit window

---
+++ Core/SubmitLogic.cs
@@ -0,0 +1,26 @@
+﻿using Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class SubmitLogic
+    {
+        //определение дня дежурства пользователя для базы данных
+        public static string GetDayOfCleaning(List<WhoWhenClean> results, User user)
+        {
+            int thisYear = DateTime.Now.Year;
+            int todayInYear = DateTime.Now.DayOfYear;
+            int dayToAdd = SwapLogic.GetMaxDayId(results, user);
+            int firstDayInGrid = todayInYear - 3;
+            int dayNextClean = firstDayInGrid + dayToAdd;
+
+            DateTime theDate = new DateTime(thisYear, 1, 1).AddDays(dayNextClean - 1);
+
+            return theDate.ToString();
+        }
+    }
+}
--- Core/SwapLogic.cs
+++ Core/SwapLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,59 @@ namespace Core
 {
     public class SwapLogic
     {
-//        public
+        //Получаем новое расписание 
+        public static List<WhoWhenClean> ChangeDays(List<WhoWhenClean> initialSchedule, int maxDay)
+        {
+            foreach (var time in initialSchedule)
+            {
+                if (time.DayId >= maxDay)
+                {
+                    time.DayId++;
+                }
+            }
+            return initialSchedule;
+        }
+
+        public static List<WhoWhenClean> ChangeUsers(List<WhoWhenClean> initialSchedule, User one, User another)
+        {
+            int dayToChangeOfOne = GetMaxDayId(initialSchedule, one);
+            int dayToChangeOfAnother = GetMaxDayId(initialSchedule, another);
+
+            int daysToNextUserClean = 6;
+
+            if (dayToChangeOfAnother < 3)
+                dayToChangeOfAnother = dayToChangeOfAnother + daysToNextUserClean;
+
+            foreach (var time in initialSchedule)
+            {
+                if (time.DayId == dayToChangeOfOne)
+                {
+                    time.UseId = another.Id;
+                }
+
+                if (time.DayId == dayToChangeOfAnother)
+                {
+                    time.UseId = one.Id;
+                }
+            }
+            return initialSchedule;
+        }
+
+        //вычленить у пользователя день с максимальным номером
+        //День следующего дежурства
+        public static int GetMaxDayId(List<WhoWhenClean> initialSchedule, User user)
+        {
+            int maxDay = 0;
+
+            foreach (var time in initialSchedule)
+            {
+                if (time.UseId == user.Id)
+                {
+                    if (time.DayId > maxDay)
+                        maxDay = time.DayId;
+                }
+            }
+            return maxDay;
+        }
     }
 }
