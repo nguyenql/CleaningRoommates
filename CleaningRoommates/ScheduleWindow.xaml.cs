1cbdd10374dd3700a031bf3a549881d7066e28de
nguyenql <endless1164@yandex.ru>
2018-06-14 20:23:46 +0300

    Schedule.DataGrid Buttons ->Icons

--- CleaningRoommates/ScheduleWindow.xaml.cs
+++ CleaningRoommates/ScheduleWindow.xaml.cs
@@ -75,13 +75,13 @@ namespace CleaningRoommates
         {
             int idOfMaxDayInGrid = 6;
             foreach (var time in results)
-            {
-                Button newButton = new Button();
+            {                
+                Image newButton = new Image();
 
                 newButton.Height = 30;
                 newButton.Width = 30;
-                newButton.BorderBrush = Brushes.DeepSkyBlue;
-                newButton.Background = Brushes.LightBlue;
+
+                newButton.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/broom_color.png"));
 
                 Grid.SetRow(newButton, time.UseId);
 
